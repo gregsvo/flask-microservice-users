ad2f151ea0342c2a43528a53c650060427f692c1
Greg Svoboda <gregsvo@gmail.com>
2017-08-30 16:24:41 -0400

    add endpoint/tests for single user retrieval

--- flask-microservices-users/project/tests/test_users.py
+++ flask-microservices-users/project/tests/test_users.py
@@ -1,5 +1,7 @@
 import json
 
+from project import db
+from project.api.models import User
 from project.tests.base import BaseTestCase
 
 class TestUserService(BaseTestCase):
@@ -77,4 +79,36 @@ class TestUserService(BaseTestCase):
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 400)
             self.assertIn('Sorry. That email already exists.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_single_user(self):
+        """Ensure get single user behaves correctly"""
+        user = User(username='greg', email='testuser@fanos.io')
+        db.session.add(user)
+        db.session.commit()
+        with self.client:
+            response = self.client.get(f'/users/{user.id}')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue('created_at' in data['data'])
+            self.assertIn('greg', data['data']['username'])
+            self.assertIn('testuser@fanos.io', data['data']['email'])
+            self.assertIn('success', data['status'])
+
+    def test_single_user_no_id(self):
+        """Ensure error is thrown if an id is not provided."""
+        with self.client:
+            response = self.client.get('/users/blah')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_single_user_incorrect_id(self):
+        """Ensure error is thrown if the id does not exist"""
+        with self.client:
+            response = self.client.get('/users/999')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
             self.assertIn('fail', data['status'])
