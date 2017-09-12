8960966841178c973227a9515e4cb760bfd69a34
Greg Svoboda <gregsvo@gmail.com>
2017-09-11 22:16:11 -0400

    CORS added

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the db
@@ -9,6 +10,9 @@ def create_app():
 
     # instantiate the app
     app = Flask(__name__)
+
+    # enable CORS
+    CORS(app)
 
     # set config
     app_settings = os.getenv('APP_SETTINGS')
